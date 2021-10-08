6c02a250f710fcff72e0302a031eb35582540cbf
Pandu dwi Putra <pandu@pandudpn.id>
2021-10-09 00:44:20 +0700

    Fix Elastic (#2)
    * add log for errorLocation and function name
    
    * add and store context to log
    
    * fix cors config and adding log before set erroLocation
    
    * remove file path. only use module path and line
    
    * fix when try to send elastic
    
    * remove waitgroup

--- middleware/record.go
+++ middleware/record.go
@@ -4,7 +4,6 @@ import (
 	"net/http"
 	"os"
 	"strconv"
-	"sync"
 	"time"
 
 	"github.com/s-pos/go-utils/logger"
@@ -30,20 +29,11 @@ func (c *client) write(dl *logger.DataLogger) {
 		level            logrus.Level
 		elasticStatus, _ = strconv.ParseBool(os.Getenv("ELASTIC_ENABLED"))
 		errChan          = make(chan error, 1)
-		wg               sync.WaitGroup
 	)
 	if elasticStatus {
-		elastic := logger.NewElastic()
-		wg.Add(1)
 		go func() {
-			defer wg.Done()
+			elastic := logger.NewElastic()
 			elastic.SendDataToElastic(dl, errChan)
-
-			select {
-			case err := <-errChan:
-				c.log.Errorf("error send data to elastic %v", err)
-			default:
-			}
 		}()
 	}
 
@@ -57,8 +47,12 @@ func (c *client) write(dl *logger.DataLogger) {
 
 	monitoring.Prometheus().Record(dl.StatusCode, dl.RequestMethod, dl.Endpoint, dl.ResponseMessage, time.Since(dl.TimeStart))
 	if elasticStatus {
-		wg.Wait()
-		close(errChan)
+		select {
+		case err := <-errChan:
+			c.log.Errorf("error send data to elastic %v", err)
+		default:
+			close(errChan)
+		}
 	}
 	c.log.WithField("incoming_log", dl).Log(level, "apps")
 }
